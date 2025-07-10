ef8327f79a9c96fe92c2fd8f5e36ad1e2ad6c6c6
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2025-07-10 17:04:26 +0545

    login system added, but password is not yet protected

--- lib/install.php
+++ lib/install.php
@@ -11,16 +11,13 @@ function installBlog(PDO $pdo)
     $database = getDatabasePath();
     $error = '';
     // A security measure, to avoid anyone resetting the database if it already exists
-    if (is_readable($database) && filesize($database) > 0)
-    {
+    if (is_readable($database) && filesize($database) > 0) {
         $error = 'Please delete the existing database manually before installing it afresh';
     }
     // Create an empty file for the database
-    if (!$error)
-    {
+    if (!$error) {
         $createdOk = @touch($database);
-        if (!$createdOk)
-        {
+        if (!$createdOk) {
             $error = sprintf(
                 'Could not create the database, please allow the server to create new files in \'%s\'',
                 dirname($database)
@@ -28,37 +25,81 @@ function installBlog(PDO $pdo)
         }
     }
     // Grab the SQL commands we want to run on the database
-    if (!$error)
-    {
+    if (!$error) {
         $sql = file_get_contents($root . '/data/init.sql');
-        if ($sql === false)
-        {
+        if ($sql === false) {
             $error = 'Cannot find SQL file';
         }
     }
     // Connect to the new database and try to run the SQL commands
-    if (!$error)
-    {
+    if (!$error) {
         $result = $pdo->exec($sql);
-        if ($result === false)
-        {
+        if ($result === false) {
             $error = 'Could not run SQL: ' . print_r($pdo->errorInfo(), true);
         }
     }
     // See how many rows we created, if any
     $count = array();
-    foreach(array('post', 'comment') as $tableName)
-    {
-        if (!$error)
-        {
+    foreach (array('post', 'comment') as $tableName) {
+        if (!$error) {
             $sql = "SELECT COUNT(*) AS c FROM " . $tableName;
             $stmt = $pdo->query($sql);
-            if ($stmt)
-            {
+            if ($stmt) {
                 // We store each count in an associative array
                 $count[$tableName] = $stmt->fetchColumn();
             }
         }
     }
     return array($count, $error);
+}
+
+/**
+ * Creates a new user in the database
+ *
+ * @param PDO $pdo
+ * @param string $username
+ * @param integer $length
+ * @return array Duple of (password, error)
+ */
+function createUser(PDO $pdo, $username, $length = 10)
+{
+    // This algorithm creates a random password
+    $alphabet = range(ord('A'), ord('z'));
+    $alphabetLength = count($alphabet);
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $letterCode = $alphabet[rand(0, $alphabetLength - 1)];
+        $password .= chr($letterCode);
+    }
+    $error = '';
+    // Insert the credentials into the database
+    $sql = "
+        INSERT INTO
+            user
+            (username, password, created_at)
+            VALUES (
+                :username, :password, :created_at
+            )
+    ";
+    $stmt = $pdo->prepare($sql);
+    if ($stmt === false) {
+        $error = 'Could not prepare the user creation';
+    }
+    // We're storing the password in plaintext, will fix that later
+    if (!$error) {
+        $result = $stmt->execute(
+            array(
+                'username' => $username,
+                'password' => $password,
+                'created_at' => getSqlDateForNow(),
+            )
+        );
+        if ($result === false) {
+            $error = 'Could not run the user creation';
+        }
+    }
+    if ($error) {
+        $password = '';
+    }
+    return array($password, $error);
 }
